1365f4305d5f3d7b39c25b612c91fca0570fd484
Rodrigue Fokam <fokrod22@gmail.com>
2023-10-11 11:10:11 +0200

    mein erstes C-programm

--- erste.c
+++ erste.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
 int main (int argc, char *argv[]){
+    printf("AAA\n");
     printf ("%d [0x%x] [%c]\n " , 42, 42, 0x42);
+
+    char buffer[100]; 
+    int nombre = 42;
+    double reel = 3.14159;
+
+    sprintf(buffer, "Ceci est un exemple de sprintf papapa\n");
+    printf( buffer);
+
+    sprintf(buffer, "Ceci est un exemple de sprintf : nombre = %d, reel = %.2f", nombre, reel);
+
+    printf (buffer);
+    printf("\nLe texte formaté est : %s\n", buffer);
     return 0;
 }
